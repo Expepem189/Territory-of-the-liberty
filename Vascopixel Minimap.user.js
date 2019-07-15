a23e638723e39e0c87785c70ee258147cf47f358
Expepem189 <52920744+Expepem189@users.noreply.github.com>
2019-07-15 16:59:28 +0300

    Update Vascopixel Minimap.user.js

--- Vascopixel Minimap.user.js
+++ Vascopixel Minimap.user.js
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://github.com/Expepem189/Territory-of-the-liberty';
+window.baseTepmlateUrl = 'https://github.com/Expepem189/Territory-of-the-liberty/master';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
